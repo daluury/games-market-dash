755960952d3f4cef33d7cadda336648f039f9e54
daluury <71202353+daluury@users.noreply.github.com>
2022-04-03 19:55:03 +1000

    корректировка графиков

--- games_market_dash.py
+++ games_market_dash.py
@@ -1,5 +1,5 @@
 import dash
-import dash_core_components as dcc
+import dash_core_components as dcc #интерактивные компоненты
 import dash_html_components as html
 import pandas as pd
 import plotly.express as px
@@ -9,10 +9,11 @@ data = pd.read_hdf('./Norm_games.h5')
 
 app = dash.Dash()
 
+# переменные с жанрами и рейтингами для фильтрации
 available_genre = data['Genre'].unique()
 available_rating = data['Rating'].unique()
 
-
+# инициализация и определение внешнего вида приложения dash
 app.layout = html.Div([
         html.Div([
             html.H1("Состояние игровой индустрии"),
@@ -26,24 +27,26 @@ app.layout = html.Div([
             'padding': '10px 5px'
         }),
 
+        # фильтр по жанрам
         html.Div([
             html.Div([
                 html.Label('Жанры игр'),
                 dcc.Dropdown(
                     id = 'crossfilter-genre',
                     options = [{'label': i, 'value': i} for i in available_genre],
-                    value = ['Sports', 'Strategy'],
+                    value = ['Sports', 'Strategy'], # значения по умолчанию
                     multi = True 
                 )
             ],
             style = {'width': '49%', 'display': 'inline-block'}),
 
+            # фильтр по рейтингам
             html.Div([
                 html.Label('Рейтинги игр'),
                 dcc.Dropdown(
                     id = 'crossfilter-rating',
                     options = [{'label': i, 'value': i} for i in available_rating],
-                    value = ['T', 'E'],
+                    value = ['T', 'E'], # значения по умолчанию
                     multi = True 
                 )
             ],
@@ -54,48 +57,31 @@ app.layout = html.Div([
             'padding': '10px 5px'
         }),
 
+        # заготовка для интерактивного текста - результата фильтрации
         html.Div(
             id = 'textarea-output',
             style = {'width': '100%', 'float': 'right', 'display': 'inline-block'}
         ), 
 
-        # html.Div([
-        #     dcc.Graph(
-        #         id = 'stacked-area-plot',
-        #         figure = px.area(
-        #             data, x = "Year_of_Release", y = "Critic_Score", color = "Platform"
-        #             # ,line_group = ""
-        #         ) 
-        #     )
-        # ],
-        # style = {'width': '49%', 'display': 'inline-block'}), 
-
-        # html.Div([
-        #     dcc.Graph(
-        #         id = 'scatter-plot',
-        #         figure = px.scatter(
-        #             data, x = "User_Score", y = "Critic_Score", color = "Genre"
-        #         )
-        #     )
-        # ],
-        # style = {'width': '49%', 'float': 'right', 'display': 'inline-block'}), 
-
+        # гистограмма
         html.Div(
-            dcc.Graph(id = 'stacked-area-plot'),
+            dcc.Graph(id = 'histogram'),
             style = {'width': '49%', 'display': 'inline-block'}
         ), 
         
+        # диаграмма рассеяния (scatter plot)
         html.Div(
             dcc.Graph(id = 'scatter-plot'),
             style = {'width': '49%', 'float': 'right', 'display': 'inline-block'}
         ), 
 
+        # фильтр по годам
         html.Div(
             dcc.Slider(
                 id = 'crossfilter-year',
                 min = data['Year_of_Release'].min(),
                 max = data['Year_of_Release'].max(),
-                value = 2008,
+                value = 2008, # значение по умолчанию
                 step = None,
                 marks = {str(year): str(year) for year in data['Year_of_Release'].unique()}
                 ), 
@@ -103,6 +89,7 @@ app.layout = html.Div([
         )
 ])
 
+# обратный вызов результата фильтрации
 @app.callback(
     Output('textarea-output', 'children'), 
     [Input('crossfilter-genre', 'value'),
@@ -118,9 +105,9 @@ def update_textarea(genre, rating, year):
     return 'Результат фильтрации: {}'.format(games_count)
 
 
-
+# обратный вызов histogram
 @app.callback(
-    Output('stacked-area-plot', 'figure'), 
+    Output('histogram', 'figure'), 
     [Input('crossfilter-genre', 'value'),
     Input('crossfilter-rating', 'value'),
     Input('crossfilter-year', 'value')])
@@ -129,13 +116,21 @@ def update_stacked_area(genre, rating, year):
     filtered_data = filtered_data[filtered_data['Genre'].isin(genre)]
     filtered_data = filtered_data[filtered_data['Rating'].isin(rating)]
 
-    figure = px.area(
-        filtered_data, x = "Year_of_Release", y = "User_Score", color = "Platform"
-        # ,line_group = ""
+    figure = px.histogram(
+        filtered_data, 
+        x = "Year_of_Release",
+        color = "Rating",
+        labels = {
+             "Year_of_Release": "Год релиза игр",
+             "Rating": "Рейтинг"
+         },
+         title = "Гистограмма рейтингов игр по годам"
     )
+    figure.update_layout(yaxis_title = "Кол-во")
 
     return figure
 
+# обратный вызов scatter plot
 @app.callback(
     Output('scatter-plot', 'figure'), 
     [Input('crossfilter-genre', 'value'),
@@ -147,7 +142,16 @@ def update_scatter(genre, rating, year):
     filtered_data = filtered_data[filtered_data['Rating'].isin(rating)]
 
     figure = px.scatter(
-         filtered_data, x = "User_Score", y = "Critic_Score", color = "Genre"
+         filtered_data, 
+         x = "User_Score", 
+         y = "Critic_Score", 
+         color = "Genre",
+         labels = {
+             "User_Score": "Оценка пользователей",
+             "Critic_Score": "Оценка критиков",
+             "Genre": "Жанр"
+         },
+         title = "Зависимость оценок от жанров"
     )
 
     return figure
